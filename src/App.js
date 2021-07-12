6634f604c63c0976ef32df92258320156f06cde4
rishik-m <rishik.makhija2000@gmail.com>
2021-07-12 09:46:49 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
 import Note from './components/note';
+import notes from './notes';
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note => {
+        return(
+          <Note
+            key= {note.key}
+            title= {note.title}
+            description= {note.content}
+          />
+        )
+      })}
       <Footer />
     </div>
   );
